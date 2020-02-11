d944b65acfa7c006271e21c0eaae22a2292e4beb
AFaurholt <a_faurholt@outlook.com>
2020-02-11 17:53:35 +0100

    renamed sample scene + added test mats + added test models + simple player move controller + input util for held keys

---
+++ Assets/Scripts/Util/InputUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.runtime.GameJamBois.BGJ20201.Util
+{
+    public static class InputUtil
+    {
+        public static Dictionary<KeyCode, bool> GetIfKeysHeld(KeyCode[] keysToCheck)
+        {
+            Dictionary<KeyCode, bool> valuePairs = new Dictionary<KeyCode, bool>();
+
+            foreach (KeyCode item in keysToCheck)
+            {
+                if (Input.GetKey(item))
+                {
+                    valuePairs.Add(item, true);
+                }
+                else
+                {
+                    valuePairs.Add(item, false);
+                }
+            }
+
+            return valuePairs;
+        }
+    }
+}
