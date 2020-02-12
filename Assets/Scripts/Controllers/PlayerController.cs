e6ca1e25469c896532b0bde9605cb11a235c67a0
AFaurholt <a_faurholt@outlook.com>
2020-02-12 19:22:52 +0100

    clamped look

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -20,6 +20,21 @@ namespace com.runtime.GameJamBois.BGJ20201.Controllers
         [SerializeField] private Camera _camera;
         [Tooltip("*Required")]
         [SerializeField] private Transform _cameraLockTransform;
+        [SerializeField] private float _cameraYMax = 90f;
+        [SerializeField] private float _cameraYMin = 0f;
+        [SerializeField] private float _cameraXMax = 90f;
+        [SerializeField] private float _cameraXMin = 0f;
+        [SerializeField] private bool _camRotationSmoothing = true;
+        [SerializeField] private Vector3 _camOffset = new Vector3(0, 0, 0);
+
+        private Vector2 _camDirection;
+        private Vector2 _playerDirection;
+
+        //TODO: refactor out
+        [Header("Mouse")]
+        [SerializeField] private bool _rawMouseInput = false;
+        [SerializeField] private float _mouseSensitivityX = 1f;
+        [SerializeField] private float _mouseSensitivityY = 1f;
 
         //last mouse position (for FPS)
         private float _rotationX = 0f, _rotationY = 0f;
@@ -48,8 +63,9 @@ namespace com.runtime.GameJamBois.BGJ20201.Controllers
 
             //set cam at lock position
             //TODO: refactor out, cam stuff
-            _camera.transform.position = _cameraLockTransform.position;
-
+            _camera.transform.position = _cameraLockTransform.position - _camOffset;
+            _camDirection = _camera.transform.localRotation.eulerAngles;
+            _playerDirection = transform.localRotation.eulerAngles;
         }
 
         // Update is called once per frame
@@ -57,13 +73,31 @@ namespace com.runtime.GameJamBois.BGJ20201.Controllers
         {
             //cam
             //TODO: refactor out
-            //TODO: add clamp for Y
-            //TODO: add clamp for X for staggered turn (no owl head)
-            //TODO: add sensitivity
-            _rotationX = _camera.transform.localEulerAngles.y + Input.GetAxis("Mouse X");
-            _rotationY += Input.GetAxis("Mouse Y");
+            //ripped from https://forum.unity.com/threads/a-free-simple-smooth-mouselook.73117/
+            float mouseY, mouseX;
+            if (_rawMouseInput)
+            {
+                mouseX = Input.GetAxisRaw("Mouse X");
+                mouseY = Input.GetAxisRaw("Mouse Y");
+            }
+            else
+            {
+                mouseX = Input.GetAxis("Mouse X");
+                mouseY = Input.GetAxis("Mouse Y");
 
-            _camera.transform.localEulerAngles = new Vector3(-_rotationY, _rotationX, 0);
+            }
+            mouseX *= _mouseSensitivityX;
+            mouseY *= _mouseSensitivityY;
+
+            _rotationX += mouseX;
+            _rotationY += mouseY;
+
+            _rotationX = Mathf.Clamp(_rotationX, _cameraXMin, _cameraXMax);
+            _rotationY = Mathf.Clamp(_rotationY, _cameraYMin, _cameraYMax);
+
+            Quaternion qCamDirection = Quaternion.Euler(_camDirection);
+            _camera.transform.localRotation = Quaternion.AngleAxis(-_rotationY, qCamDirection * Vector3.right) * qCamDirection;
+            _camera.transform.localRotation *= Quaternion.AngleAxis(_rotationX, _camera.transform.InverseTransformDirection(Vector3.up));
 
             //movement
             _currentKeys = InputUtil.GetIfKeysHeld(_controls);
@@ -74,8 +108,9 @@ namespace com.runtime.GameJamBois.BGJ20201.Controllers
             SimpleMoveByForce();
 
             //TODO: refactor cam stuff
-            _camera.transform.position = _cameraLockTransform.position;
-            transform.rotation = _camera.transform.rotation;
+            _camera.transform.position = _cameraLockTransform.position - _camOffset;
+
+            //transform.rotation = _camera.transform.rotation;
         }
         /// <summary>
         /// Simple <see cref="UnityEngine.Rigidbody.AddRelativeForce(Vector3, ForceMode)"/> based on facing. Reads controls from <see cref="_currentKeys"/>
@@ -104,6 +139,25 @@ namespace com.runtime.GameJamBois.BGJ20201.Controllers
                 }
             }
 
+        }
+
+        /// <summary>
+        /// Clamps the rotation around x-axis. Taken from the Unity Standard Assets Firsperson Controller (I don't understand Quaternions)
+        /// </summary>
+        /// <param name="q">The rotation</param>
+        /// <returns>The rotation clamped by <see cref="_cameraXMax"/> and <see cref="_cameraXMin"/></returns>
+        Quaternion ClampRotationAroundXAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+            angleX = Mathf.Clamp(angleX, _cameraXMin, _cameraXMax);
+            q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+            return q;
         }
     }
 }
