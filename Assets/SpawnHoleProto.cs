5b5c9a7970e3744e0cfc110f3b10c5aada8ec55c
iv2b <54509370+iv2b@users.noreply.github.com>
2020-02-16 12:30:49 +0100

    prototype to spawn black holes and release them in fps, has issue with body rotation

---
+++ Assets/SpawnHoleProto.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHoleProto : MonoBehaviour
+{
+    public GameObject holePrefab;
+    GameObject tempHole;
+    public float maxSize = 1;
+    float size = 0;
+
+    public float timeToMaxSize = 2;
+    float time = 0;
+
+    bool isCharging = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isCharging)
+            {
+                tempHole = Instantiate(holePrefab,this.transform);
+                tempHole.transform.localScale = new Vector3(0, 0, 0);
+                time = 0;
+                size = 0;
+                isCharging = true;
+            }
+            else
+            {
+                time += Time.deltaTime;
+                size = Mathf.Min(time / timeToMaxSize * maxSize, maxSize);
+                Debug.Log(size);
+                tempHole.transform.localScale = new Vector3(size,size,size);
+            }
+        }
+        else
+        {
+            if (isCharging)
+            {
+                tempHole.transform.parent = null;
+                tempHole = null;
+                isCharging = false;
+            }
+        }
+    }
+
+    
+}
