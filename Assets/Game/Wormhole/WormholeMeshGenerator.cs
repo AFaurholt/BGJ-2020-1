352e5b1d5059be1f77ccef6eea7d0ee691a78c2e
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-02-17 20:36:54 +0100

    Added wiggle to wormhole

--- Assets/Game/Wormhole/WormholeMeshGenerator.cs
+++ Assets/Game/Wormhole/WormholeMeshGenerator.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public static class WormholeMeshGenerator
 {
-    private const int trisPerSegment = 2;
+    private const int trisPerSegment = 4;
 
-    internal static Mesh GetCylinder(int resolution, int length, float radius)
+    internal static Mesh GetCylinder(int resolution, int length, float radius, float randomRotation)
     {
         Vector3[] verts = new Vector3[resolution * length];
         Vector3[] normals = new Vector3[verts.Length];
+
+        Vector3 currentPosition = Vector3.zero;
+        Vector3 currentDirection = Vector3.forward;
+        Vector3 currentUp = Vector3.up;
         for (int i = 0; i < length; i++)
         {
-            Vector3 position = new Vector3(0, 0, i);
-            FillCircle(position, radius, resolution, verts, normals, i * resolution);
+            FillCircle(currentPosition, currentUp, currentDirection, radius, resolution, verts, normals, i * resolution);
+            currentPosition += currentDirection;
+
+            Quaternion randomQuaternion = Quaternion.Euler(
+                Random.Range(-randomRotation, randomRotation),
+                Random.Range(-randomRotation, randomRotation), 
+                0);
+            currentDirection = randomQuaternion * currentDirection;
+            currentUp = randomQuaternion * currentUp;
+
         }
 
         int[] tris = new int[(resolution * (length - 1)) * trisPerSegment * 3];
@@ -27,13 +38,21 @@ public static class WormholeMeshGenerator
                 int vertPos = ring * resolution + vert;
                 int trisPos = vertPos * trisPerSegment * 3;
 
-                tris[trisPos + 0] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
-                tris[trisPos + 1] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
-                tris[trisPos + 2] = GetIndexOnCylinder(vertPos, 1, 0, resolution);
+                tris[trisPos + 0 ] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
+                tris[trisPos + 1 ] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
+                tris[trisPos + 2 ] = GetIndexOnCylinder(vertPos, 1, 0, resolution);
 
-                tris[trisPos + 3] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
-                tris[trisPos + 4] = GetIndexOnCylinder(vertPos, 0, 1, resolution);
-                tris[trisPos + 5] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
+                tris[trisPos + 3 ] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
+                tris[trisPos + 4 ] = GetIndexOnCylinder(vertPos, 0, 1, resolution);
+                tris[trisPos + 5 ] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
+
+                tris[trisPos + 6 ] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
+                tris[trisPos + 7 ] = GetIndexOnCylinder(vertPos, 1, 0, resolution);
+                tris[trisPos + 8 ] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
+
+                tris[trisPos + 9 ] = GetIndexOnCylinder(vertPos, 0, 0, resolution);
+                tris[trisPos + 10] = GetIndexOnCylinder(vertPos, 1, 1, resolution);
+                tris[trisPos + 11] = GetIndexOnCylinder(vertPos, 0, 1, resolution);
             }
         }
 
@@ -45,19 +64,21 @@ public static class WormholeMeshGenerator
         };
     }
 
-    private static void FillCircle(Vector3 position, float radius, int resolution, Vector3[] verts, Vector3[] normals, int startingIndex)
+    private static void FillCircle(Vector3 position, Vector3 up, Vector3 forward, float radius, int resolution, Vector3[] verts, Vector3[] normals, int startingIndex)
     {
-        Vector3 direction = Vector3.up;
-        Quaternion rotationPerStep = Quaternion.Euler(0, 0, 360f / resolution);
+        forward.Normalize();
+        up.Normalize();
+
+        Quaternion rotationPerStep = Quaternion.AngleAxis(360f / resolution, forward);
 
         for (int i = 0; i < resolution; i++)
         {
             int currentIndex = startingIndex + i;
 
-            verts[currentIndex] = position + direction * radius;
-            normals[currentIndex] = -direction;
+            verts[currentIndex] = position + up * radius;
+            normals[currentIndex] = -up;
 
-            direction = rotationPerStep * direction;
+            up = rotationPerStep * up;
         }
     }
 
--- Assets/WormholeRenderer.cs
+++ Assets/WormholeRenderer.cs
@@ -8,14 +8,18 @@ public class WormholeRenderer : MonoBehaviour
     [SerializeField] private MeshFilter filter;
     [Space]
     [SerializeField, Range(4, 90)] private int resolution = 10;
-    [SerializeField, Range(2, 10)] private int length = 5;
+    [SerializeField, Range(2, 90)] private int length = 5;
     [SerializeField, Range(0.2f, 5)] private float radius = 1;
+    [Space]
+    [SerializeField, Range(0, 10f)] private float randomRotation = 2;
+    [SerializeField] private int seed;
 
     private void Update()
     {
         if(filter != null)
         {
-            Mesh mesh = WormholeMeshGenerator.GetCylinder(resolution, length, radius);
+            Random.InitState(seed);
+            Mesh mesh = WormholeMeshGenerator.GetCylinder(resolution, length, radius, randomRotation);
             filter.mesh = mesh;
         }
     }
