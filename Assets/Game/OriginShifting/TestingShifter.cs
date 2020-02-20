f19e8078d9f6bf961e7b5cb98136e918737e2597
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-02-20 19:42:58 +0100

    Added origin shifting

---
+++ Assets/Game/OriginShifting/TestingShifter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingShifter : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+
+    void Update()
+    {
+        OriginShifter.MoveOriginBy(speed * Vector3.forward * Time.deltaTime);       
+    }
+}
