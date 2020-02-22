cfe8e2322382dc04f1df26a06fad0e944b252ee6
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-02-22 22:53:51 +0100

    Added main menu functionality

---
+++ Assets/GameContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameContext : MonoBehaviour
+{
+    public PlayableDirector IntroDirector;
+    public GibbingScript HeadGibber;
+    public DifficultyManager DifficultyManager;
+
+    public static GameContext Current = null;
+
+    private void OnEnable()
+    {
+        if (Current != null)
+            Debug.LogWarning($"{name} will override {Current.name}", this);
+        Current = this;
+    }
+
+    private void OnDisable()
+    {
+        if (Current == this)
+            Current = null;
+    }
+}
---
+++ Assets/SceneUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public class SceneUtils
+{
+    public static void LoadScene(string name)
+    {
+        Scene scene = SceneManager.GetSceneByName(name);
+        if (scene.IsValid())
+        {
+            return;
+        }
+        else
+        {
+            SceneManager.LoadScene(name, LoadSceneMode.Additive);
+        }
+    }
+
+    public static void UnloadScene(string name)
+    {
+        SceneManager.UnloadSceneAsync(name);
+    }
+}
